ffb40e99bf7a9eee4ca50995d904631d12795d15
Tetsu-is <hap30305@kwansei.ac.jp>
2024-03-18 04:52:38 +0900

    add button from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 const Home = () => {
+  const handleReadReceipt = () => {
+    console.log("Read Receipt");
+  };
   return (
-    <main className="flex flex-col items-center p-10 gap-5">
+    <main className="flex flex-col items-center py-10 gap-5">
       <h1 className="text-[30px]">読み込み</h1>
-      <article className="w-[375px] h-[550px] bg-gray-500">Camera Field</article>
+      <article className="w-[90%] h-[450px] bg-gray-500">Camera Field</article>
+      <Button variant={"outline"} onClick={handleReadReceipt}>
+        レシートを読み込む
+      </Button>
+      <Button variant={"outline"}>
+        <Link href="/history">履歴</Link>
+      </Button>
     </main>
   );
-}
+};
 
 export default Home;
