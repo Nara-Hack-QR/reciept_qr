2839498eae061188ae32e979cab37f958db05e52
Tetsu-is <hap30305@kwansei.ac.jp>
2024-03-18 15:38:30 +0900

    define type for receipt

---
+++ src/lib/type.ts
@@ -0,0 +1,13 @@
+type Commodity = {
+    name: string;
+    price: number;
+    count: number;
+};
+
+type Receipt = {
+    date: string;
+    commodities: Commodity[]; 
+    publisherName: string;
+    totalPrice: number;
+    publisherAddress: string;
+};
